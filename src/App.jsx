9c4b0e9f63502055a3472e6cddb14cc84197911d
Brayan <brayanbracho1515@gmail.com>
2025-09-28 18:31:30 -0400

    se hizo el carrito de compra

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext.jsx';
 import Header from './Header.jsx';
 
 import Home from './pages/Home/Home.jsx';
@@ -9,17 +10,19 @@ import Reset_Password from './pages/Reset_Password/Reset_Password.jsx';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot_password" element={<Forgot_Password />} />
-        <Route path="/reset_password" element={<Reset_Password />} />
-        <Route path="/reset-password" element={<Reset_Password />} />
-      </Routes>
-    </Router>
+    <CartProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot_password" element={<Forgot_Password />} />
+          <Route path="/reset_password" element={<Reset_Password />} />
+          <Route path="/reset-password" element={<Reset_Password />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
 
